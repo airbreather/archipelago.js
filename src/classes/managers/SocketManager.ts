21f40e3fec9402ac3f968bd5bd06c55a37d9dfee
Joe Amenta <airbreather@linux.com>
2026-04-15 17:29:24 -0400

    how hard is it really supposed to be to find WebSocket?

--- src/classes/managers/SocketManager.ts
+++ src/classes/managers/SocketManager.ts
@@ -198,26 +198,32 @@ export class SocketManager extends EventBasedManager<SocketEvents> {
 
     #findWebSocket(): typeof WebSocket | null {
         let IsomorphousWebSocket: typeof WebSocket | null = null;
-        if (typeof window !== "undefined") {
-            // @ts-expect-error WebSocket may not exist in this context.
-            IsomorphousWebSocket = window.WebSocket || window.MozWebSocket;
-            console.log("typeof window !== \"undefined\"");
-        } else if (typeof global !== "undefined") {
-            // @ts-expect-error WebSocket may not exist in this context.
-            IsomorphousWebSocket = global.WebSocket || global.MozWebSocket;
-            console.log("typeof window !== \"undefined\"");
-        } else if (typeof self !== "undefined") {
-            // @ts-expect-error WebSocket may not exist in this context.
-            IsomorphousWebSocket = self.WebSocket || self.MozWebSocket;
-            console.log("typeof window !== \"undefined\"");
-        } else if (typeof WebSocket !== "undefined") {
-            IsomorphousWebSocket = WebSocket;
-            console.log("typeof window !== \"undefined\"");
-            // @ts-expect-error WebSocket may not exist in this context.
-        } else if (typeof MozWebSocket !== "undefined") {
-            // @ts-expect-error WebSocket may not exist in this context.
-            IsomorphousWebSocket = MozWebSocket as WebSocket;
-            console.log("typeof window !== \"undefined\"");
+        if (typeof globalThis !== "undefined") {
+            if (typeof globalThis.WebSocket !== "undefined") {
+                IsomorphousWebSocket = globalThis.WebSocket;
+                console.log("typeof globalThis.WebSocket !== \"undefined\"");
+                // @ts-expect-error WebSocket may not exist in this context.
+            } else if (typeof globalThis.MozWebSocket !== "undefined") {
+                // @ts-expect-error WebSocket may not exist in this context.
+                IsomorphousWebSocket = MozWebSocket as WebSocket;
+                console.log("typeof globalThis.MozWebSocket !== \"undefined\"");
+            }
+        }
+
+        if (!IsomorphousWebSocket) {
+            if (typeof window !== "undefined") {
+                // @ts-expect-error WebSocket may not exist in this context.
+                IsomorphousWebSocket = window.WebSocket || window.MozWebSocket;
+                console.log("typeof window !== \"undefined\"");
+            } else if (typeof global !== "undefined") {
+                // @ts-expect-error WebSocket may not exist in this context.
+                IsomorphousWebSocket = global.WebSocket || global.MozWebSocket;
+                console.log("typeof window !== \"undefined\"");
+            } else if (typeof self !== "undefined") {
+                // @ts-expect-error WebSocket may not exist in this context.
+                IsomorphousWebSocket = self.WebSocket || self.MozWebSocket;
+                console.log("typeof window !== \"undefined\"");
+            }
         }
 
         console.log("WebSocket implementation found:", util.inspect(IsomorphousWebSocket, { depth: 10 }));
