17acf4a4d808e0e2148c910475c5870671c05d06
Joe Amenta <airbreather@linux.com>
2026-04-18 12:17:16 -0400

    Overhaul ESLint configuration, trying to stay true to the original goals:
    1. Remove redundant declarations.
    2. Move ignores to their own section to make it more obvious that they're applied globally, not scoped.
    3. Explicitly identify the file extensions that are affected by each sub-configuration.
    4. Configure the stylistic and jsdoc plugins using their provided factory functions.
    5. As much as possible, express "that config, but ..." using just `extends` for consistency and clarity.
    6. Bring in a plugin for `package.json`. I can discern no intention behind the order of properties in this file.
    7. Pull in jiti, which is needed for ESLint to run on things other than Bun or Deno. In other words, `npx eslint` works again, in case that's your thing.
    
    The `eslint` script should now run as-is without any further arguments.
    
    MANY files now yield failures that were not caught by earlier attempts, with more than 300 errors about missing semicolons.
    
    Before applying the rules to address these failures, the next commit on this branch will adjust the configuration to account for apparent preferences that conflict with the defaults brought in by doing things this way.

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,64 +1,73 @@
-import { defineConfig } from 'eslint/config';
 import eslint from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
 import vitest from "@vitest/eslint-plugin";
-import jsdoc from "eslint-plugin-jsdoc";
+import { defineConfig } from "eslint/config";
+import { jsdoc } from "eslint-plugin-jsdoc";
+import packageJson from "eslint-plugin-package-json";
 import importSort from "eslint-plugin-simple-import-sort";
 import tslint from "typescript-eslint";
 
 export default defineConfig(
-    eslint.configs.recommended,
-    ...tslint.configs.recommendedTypeChecked,
-    stylistic.configs.recommended,
-    jsdoc.configs["flat/recommended-typescript"],
     {
         ignores: ["node_modules", "dist", "docs", "coverage"],
+    },
+    {
+        files: ["**/*.ts"],
+        extends: [
+            eslint.configs.recommended,
+            tslint.configs.recommendedTypeChecked,
+            tslint.configs.stylisticTypeChecked,
+            stylistic.configs.customize({
+                arrowParens: true,
+                braceStyle: "1tbs",
+                indent: 4,
+                quotes: "double",
+                semi: true,
+            }),
+            jsdoc({
+                config: "flat/recommended-typescript",
+                rules: {
+                    "jsdoc/check-tag-names": ["warn", { definedTags: ["remarks", "category", "experimental"] }],
+                    "jsdoc/require-description": "warn",
+                    "jsdoc/require-template": "warn",
+                    "jsdoc/require-returns": "off",
+                    "jsdoc/require-throws": "warn",
+                    "jsdoc/require-throws-type": "off",
+                    "jsdoc/sort-tags": "warn",
+                    "jsdoc/valid-types": "off",
+                },
+            }),
+        ],
         plugins: {
-            "@stylistic": stylistic,
             "simple-import-sort": importSort,
         },
         rules: {
-            "@stylistic/arrow-parens": ["error", "always"],
-            "@stylistic/brace-style": ["error", "1tbs"],
-            "@stylistic/indent": ["error", 4],
-            "@stylistic/indent-binary-ops": ["error", 4],
             "@stylistic/operator-linebreak": ["error", "after", {
                 overrides: {
                     "|": "before",
                 },
             }],
-            "@stylistic/quotes": ["error", "double"],
-            "@stylistic/semi": ["error", "always"],
-            "jsdoc/check-tag-names": ["warn", { definedTags: ["remarks", "category", "experimental"] }],
-            "jsdoc/require-description": "warn",
-            "jsdoc/require-template": "warn",
-            "jsdoc/require-returns": "off",
-            "jsdoc/require-throws": "warn",
-            "jsdoc/require-throws-type": "off",
-            "jsdoc/sort-tags": "warn",
-            "jsdoc/valid-types": "off",
             "simple-import-sort/imports": "error",
             "simple-import-sort/exports": "error",
         },
         languageOptions: {
-            ecmaVersion: "latest",
-            sourceType: "module",
             parserOptions: {
-                projectService: {
-                    allowDefaultProject: ["*.js", "*.mjs"],
-                },
-                tsconfigRootDir: import.meta.dirname,
+                projectService: true,
             },
         },
     },
     {
-        files: ["test/**"],
-        plugins: {
-            vitest,
-        },
-        rules: {
-            ...vitest.configs.recommended.rules,
-        },
+        files: ["package.json"],
+        extends: [
+            packageJson.configs.recommended,
+            packageJson.configs.stylistic,
+        ],
+    },
+    {
+        files: ["test/**/*.ts"],
+        extends: [
+            vitest.configs.recommended,
+        ],
         settings: {
             vitest: {
                 typecheck: true,
