06041941c2f2f375b01a1c2d4510b2f7aa1db964
Joe Amenta <airbreather@linux.com>
2026-04-17 22:54:46 -0400

    Get a different fix for ThePhar#232 - there are quite a few more edge cases in here than either ThePhar#236 or 6047b13 handled.
    Let's make this the first interesting use of the integration tests.

--- test/integration/connect.test.ts
+++ test/integration/connect.test.ts
@@ -1,27 +1,24 @@
-import { afterAll, describe, expect, test } from "vitest";
+import { afterEach, describe, expect, test } from "vitest";
 
 import { Client } from "../../src";
 
-describe("smoke tests", () => {
+describe("smoke tests", { tags: ["integration"], timeout: 5000 }, () => {
+    const url = "ws://localhost:38281";
+    const slotName = "APQuestPlayer";
+
     const client = new Client();
-    afterAll(() => client.socket.disconnect());
+    client.socket.on("sentPackets", (...args) => console.log("sent", ...args));
+    client.socket.on("receivedPacket", (...args) => console.log("received", ...args));
+    afterEach(() => client.socket.disconnect());
 
-    test("smoke test", { tags: ["integration"] }, async () => {
-        const url = "ws://localhost:38281";
-        const slotName = "APQuestPlayer";
-
+    test("smoke test", async () => {
         expect(await client.login(url, slotName, "APQuest")).not.toBeFalsy();
 
         const unlikelyMessage = "it is unlikely that someone would send this message especially with this speling error";
 
-        const messageReceived = new Promise<void>((resolve, reject) => {
-            const timeout = setTimeout(() => {
-                reject(new Error("Timeout waiting for message response"));
-            }, 5000);
-
+        const messageReceived = new Promise<void>((resolve) => {
             client.messages.on("message", (text) => {
                 if (text.includes(unlikelyMessage)) {
-                    clearTimeout(timeout);
                     resolve();
                 }
             });
@@ -29,5 +26,27 @@ describe("smoke tests", () => {
 
         await client.messages.say(unlikelyMessage);
         await messageReceived;
+
+        await client.messages.say("!admin login hunter2");
+        await client.messages.say("!admin haha nice");
+    });
+
+    test.each([
+        "just say anything, yo!",
+        "!admin",
+        "!admin ",
+        "!admin login hunter2",
+        "!admin login *******",
+        "!admin /option ",
+        "!admin /option server_password",
+        "!admin /option server_password hunter2",
+        "!admin /option server_password *******",
+    ] as const)("echo admin command test (ThePhar#232), command: %s", { tags: ["integration"] }, async (command) => {
+        expect(await client.login(url, slotName, "APQuest")).not.toBeFalsy();
+
+        // test for ThePhar#232: admin commands go through a different processor, which can cause the message we get
+        // back to differ from the message that we sent, so older versions would hang forever when you tried to send
+        // certain messages starting with "!admin" because they would never see the echo back.
+        await client.messages.say(command);
     });
 });
