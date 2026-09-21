905efa26125c5063766e39dfc12bc2f969cf5bd1
Joe Amenta <airbreather@linux.com>
2026-09-21 08:20:38 -0400

    Fix old-version documentation building:
    1. Source URLs now generate correctly.
    2. Uses the old README files.
    3. Uses the version's prose documentation files (or none if there were none).
    4. Use the right version number in the title, banner, index page, and wherever else the utility sees fit to display it.

--- rebuild-docs-with-history.bash
+++ rebuild-docs-with-history.bash
@@ -14,8 +14,13 @@ mkdir docs
 rm -rf old-version-docs
 git clone . old-version-docs
 cd old-version-docs
-rm -rf src test
-git checkout 1.0.0 -- src
+git remote set-url origin https://github.com/airbreather/archipelago.js.git
+rm -rf src test guides
+jq 'del(.projectDocuments)|.gitRevision = "5c1ed7691875a574daa4a9844eee25390b2d1e56"' typedoc.json > /tmp/t.json
+mv /tmp/t.json typedoc.json
+git checkout 5c1ed7691875a574daa4a9844eee25390b2d1e56 -- src README.md
+jq '.version = "1.0.0"' package.json > /tmp/t.json
+mv /tmp/t.json package.json
 bun i -D @types/ws@8.5.5
 bun i -D isomorphic-ws@5.0.0
 bun i -D @types/uuid@9.0.2
@@ -28,8 +33,13 @@ mv $(realpath old-version-docs/docs/stable) docs/v1.0.0
 rm -rf old-version-docs
 git clone . old-version-docs
 cd old-version-docs
-rm -rf src test
-git checkout 1.1.0 -- src
+git remote set-url origin https://github.com/airbreather/archipelago.js.git
+rm -rf src test guides
+jq 'del(.projectDocuments)|.gitRevision = "f2a4b2ed2fad44e9af2e59be8d0c0614fd38fffe"' typedoc.json > /tmp/t.json
+mv /tmp/t.json typedoc.json
+git checkout f2a4b2ed2fad44e9af2e59be8d0c0614fd38fffe -- src README.md
+jq '.version = "1.1.0"' package.json > /tmp/t.json
+mv /tmp/t.json package.json
 bun i -D @types/ws@8.5.5
 bun i -D isomorphic-ws@5.0.0
 bun i -D @types/uuid@9.0.2
@@ -42,8 +52,13 @@ mv $(realpath old-version-docs/docs/stable) docs/v1.1.0
 rm -rf old-version-docs
 git clone . old-version-docs
 cd old-version-docs
-rm -rf src test
-git checkout 1.2.0 -- src
+git remote set-url origin https://github.com/airbreather/archipelago.js.git
+rm -rf src test guides
+jq 'del(.projectDocuments)|.gitRevision = "21370f41983fdb3ed3617acb43a237af0cb13d4d"' typedoc.json > /tmp/t.json
+mv /tmp/t.json typedoc.json
+git checkout 21370f41983fdb3ed3617acb43a237af0cb13d4d -- src README.md
+jq '.version = "1.2.0"' package.json > /tmp/t.json
+mv /tmp/t.json package.json
 bun i -D @types/ws@8.5.13
 bun i -D isomorphic-ws@5.0.0
 bun i -D @types/uuid@9.0.8
@@ -56,8 +71,13 @@ mv $(realpath old-version-docs/docs/stable) docs/v1.2.0
 rm -rf old-version-docs
 git clone . old-version-docs
 cd old-version-docs
+git remote set-url origin https://github.com/airbreather/archipelago.js.git
 rm -rf src test
-git checkout c582895ca560ec66ba0bd6ed4b8214a01b0e88d7 -- src
+jq '.gitRevision = "c582895ca560ec66ba0bd6ed4b8214a01b0e88d7"' typedoc.json > /tmp/t.json
+mv /tmp/t.json typedoc.json
+git checkout c582895ca560ec66ba0bd6ed4b8214a01b0e88d7 -- src README.md guides
+jq '.version = "2.0.4"' package.json > /tmp/t.json
+mv /tmp/t.json package.json
 # v2 had accidentally changed the lower-bound to ES2025 with a reduce() call on
 # a MapIterator. we fixed that later, but we need tsconfig.json to agree.
 jq '.compilerOptions.lib = ["ES2025", "DOM"]' tsconfig.json > /tmp/tsc.json
@@ -71,8 +91,13 @@ mv $(realpath old-version-docs/docs/stable) docs/v2.0.4
 rm -rf old-version-docs
 git clone . old-version-docs
 cd old-version-docs
+git remote set-url origin https://github.com/airbreather/archipelago.js.git
 rm -rf src test
-git checkout 2.1.0 -- src
+jq '.gitRevision = "e4f692715efa87c9a69375da5a55e1c71833e07c"' typedoc.json > /tmp/t.json
+mv /tmp/t.json typedoc.json
+git checkout e4f692715efa87c9a69375da5a55e1c71833e07c -- src README.md guides
+jq '.version = "2.1.0"' package.json > /tmp/t.json
+mv /tmp/t.json package.json
 # v2 had accidentally changed the lower-bound to ES2025 with a reduce() call on
 # a MapIterator. we fixed that later, but we need tsconfig.json to agree.
 jq '.compilerOptions.lib = ["ES2025", "DOM"]' tsconfig.json > /tmp/tsc.json
@@ -82,6 +107,6 @@ cd ..
 mv $(realpath old-version-docs/docs/stable) docs/v2.1.0
 
 
-# current (v2.2.0)
+# current (v2.2.1)
 rm -rf old-version-docs
 bun run docs
